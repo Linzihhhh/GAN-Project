2953454f39358e5bba98c33f3381d52892939275
Linzihhhh <88263538+Linzihhhh@users.noreply.github.com>
2023-01-25 20:53:57 +0800

    Add batcher
    Add batcher

--- GAN.py
+++ GAN.py
@@ -5,7 +5,18 @@ import torch.nn as nn
 class Generator(nn.Module):
     def __init__(self):
         super(Generator,self).__init__()
+        self.net=nn.Sequential(
+            nn.Linear(5,100),
+            nn.ReLU(),
+            nn.Linear(100,3000),
+            nn.ReLU(),
+            nn.Linear(3000,120000),
+            nn.ReLU(),
+            nn.Linear(120000,480000),
+            )
     def forward(self,x):
+        x=self.net(x)
+        return x
         
         
 
@@ -14,15 +25,16 @@ class Discriminator(nn.Module):
         super(Discriminator,self).__init__()
         self.net=nn.Sequential(
             nn.Linear(480000,120000),
-            nn.ReLU()
+            nn.ReLU(),
             nn.Linear(120000,3000),
-            nn.ReLU()
+            nn.ReLU(),
             nn.Linear(3000,500),
-            nn.RELU()
+            nn.ReLU(),
             nn.Linear(500,1)
             )
-        
-        
     def forward(self,x):
-        
-        
+        x=self.net(x)
+        return x
+
+lr_G=0.001
+lr_D=0.001
